9b09a498fd7d06dd82659503a683ed271c0648d5
GeoSolutions <afg@geo-solutions.it>
2020-04-24 14:19:53 +0430

    - Restore Scripts and Settings

---
+++ afg/br/restore.sh
@@ -0,0 +1,47 @@
+#!/bin/sh
+
+# Exit script in case of error
+set -e
+
+echo "-----------------------------------------------------"
+echo "STARTING RESTORE $(date)"
+echo "-----------------------------------------------------"
+
+if [ "$1" != "" ]; then
+    BKP_FOLDER_NAME="$1"
+else
+    BKP_FOLDER_NAME="data/backup_restore"
+fi
+
+if [ -z "$SOURCE_URL" ] || [ -z "$TARGET_URL" ]
+then
+    echo "-----------------------------------------------------"
+    echo "ERROR: SOURCE_URL and TARGET_URL environment variables not set"
+    echo " e.g.: SOURCE_URL=https://disasterrisk.af TARGET_URL=https://stage1.disasterrisk.af"
+    echo "-----------------------------------------------------"
+    exit 1
+else
+    echo "$SOURCE_URL --> $TARGET_URL"
+fi
+
+cd /home/geosolutions/venv/afg
+
+BKP_FILE_LATEST=$(find /$BKP_FOLDER_NAME/*.zip -type f -exec stat -c '%Y %n' {} \; | sort -nr | awk 'NR==1,NR==1 {print $2}')
+BKP_FILE_NAME=$(echo $BKP_FILE_LATEST | tail -n 1 | grep -oP -m 1 "\/$BKP_FOLDER_NAME\/\K.*" | sed 's|.zip||')
+
+if md5sum -c /$BKP_FOLDER_NAME/$BKP_FILE_NAME.md5; then
+    # The MD5 sum matched
+    ./manage.sh restore -c afg/br/settings_afg.ini -n -f --backup-file /$BKP_FOLDER_NAME/$BKP_FILE_NAME.zip
+    ./manage.sh migrate_baseurl -f --source-address=$SOURCE_URL --target-address=$TARGET_URL
+    ./manage.sh set_all_layers_metadata -d -i
+else
+    # The MD5 sum didn't match
+    echo "-----------------------------------------------------"
+    echo "ERROR: The MD5 sum didn't match"
+    echo "-----------------------------------------------------"
+    exit 1
+fi
+
+echo "-----------------------------------------------------"
+echo "FINISHED RESTORE $(date)"
+echo "-----------------------------------------------------"
